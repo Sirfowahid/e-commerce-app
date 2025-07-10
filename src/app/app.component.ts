64527f250cf6b71dd869087a960438bc66793282
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-10 15:51:02 +0600

    Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,BrandComponent],
+  imports: [RouterOutlet,HeaderComponent,BrandComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
