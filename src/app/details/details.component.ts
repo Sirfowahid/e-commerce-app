96f0ab3bf6294de48158c22b40b27a6942919cbe
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-11 10:29:59 +0600

    Populate details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css']  
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.http.get(`https://fakestoreapi.com/products/${id}`).subscribe({
+        next: (data) => {
+          console.log('Product details:', data);
+        },
+        error: (err) => {
+          console.error('Failed to fetch product details', err);
+        }
+      });
+    }
+  }
 }
