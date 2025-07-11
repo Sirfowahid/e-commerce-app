2695d2c3ac98a0b9696331bbb2b60f6872cb5c71
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-11 10:11:33 +0600

    Adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'product/:id', component: DetailsComponent},
+    { path: 'cart', component: CartComponent}
+];
