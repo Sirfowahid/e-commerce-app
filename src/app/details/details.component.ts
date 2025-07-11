e80d2988bde1945dbfa0d7b3744810e9106554ea
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-11 11:17:55 +0600

    Add hard coded cart details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
@@ -13,7 +14,6 @@ export class DetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
   private http = inject(HttpClient);
 
-  // Define a signal to hold the product data, initially null
   product = signal<any>(null);
 
   ngOnInit(): void {
@@ -21,7 +21,7 @@ export class DetailsComponent implements OnInit {
     if (id) {
       this.http.get(`https://fakestoreapi.com/products/${id}`).subscribe({
         next: (data) => {
-          // Update the signal with fetched data
+          
           this.product.set(data);
           console.log('Product details fetched:', data);
         },
