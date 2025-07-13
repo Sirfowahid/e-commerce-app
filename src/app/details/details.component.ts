1a688f5f0aa832b92666ae8f7cbbe289ca17f51d
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-13 14:02:54 +0600

    Use service for product details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,40 +1,41 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterModule } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { Items } from '../models/Items.model';
+import { ProductsService } from '../core/services/products.service';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
 
 export class DetailsComponent implements OnInit {
   private route = inject(ActivatedRoute);
-  private http = inject(HttpClient);
-  public quantity = signal<number>(1);
+  private productService = inject(ProductsService); 
 
   product = signal<Items | null>(null);
+  quantity = signal<number>(1);
 
-  onQuantityIncrease(){
+  onQuantityIncrease() {
     this.quantity.update(value => value + 1);
   }
 
-  onQuantityDecrease(){
+  onQuantityDecrease() {
     if (this.quantity() > 1) {
       this.quantity.update(value => value - 1);
     }
   }
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.http.get<Items>(`https://fakestoreapi.com/products/${id}`).subscribe({
-        next: (data) => {
-          
+    const idParam = this.route.snapshot.paramMap.get('id');
+    const id = idParam ? +idParam : null;
+
+    if (id !== null) {
+      this.productService.getById(id).subscribe({
+        next: (data: Items) => {
           this.product.set(data);
           console.log('Product details fetched:', data);
         },
