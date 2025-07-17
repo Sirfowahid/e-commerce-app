84e4a46ead7fb8548d732f7a5b4e9af59d70f07d
Mehedi Hasan <mehedii2912@gmail.com>
2025-07-17 11:10:48 +0600

    Add search modal

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,37 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CartService } from '../../core/services/cart.service';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   cartService = inject(CartService);
+
+  showSearch = signal(false);
+
+  searchControl = new FormControl('');
+  
+  openSearch() {
+    this.showSearch.set(true);
+  }
+
+  closeSearch() {
+    this.showSearch.set(false);
+  }
+
+  submitSearch() {
+    const query = this.searchControl.value?.trim();
+    if (query) {
+      console.log('Searching for:', query);
+      this.searchControl.setValue('');
+      this.showSearch.set(false);
+    }
+  }
 }
